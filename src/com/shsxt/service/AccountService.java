d35ea0a3f36bf1c09c157a157f3f3d05022e3428
jackson <18096652696@163.com>
2018-07-02 00:23:58 +0800

    项目初始化

---
+++ src/com/shsxt/service/AccountService.java
@@ -0,0 +1,11 @@
+package com.shsxt.service;
+
+/**
+ * Created by Administrator on 2018/7/2 0002.
+ */
+public class AccountService {
+
+    public static void main(String[] args) {
+        System.out.println("Account");
+    }
+}
---
+++ src/com/shsxt/service/UserService.java
@@ -0,0 +1,11 @@
+package com.shsxt.service;
+
+/**
+ * Created by Administrator on 2018/7/1 0001.
+ */
+public class UserService {
+    public static void main(String[] args) {
+
+        System.out.println("将项目导入git仓库中");
+    }
+}
